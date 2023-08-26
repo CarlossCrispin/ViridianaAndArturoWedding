8c147b612f070e5084b432d20840d47554bb4ce8
Carlosscrispin <carlos.crispin.cc@gmail.com>
2023-08-25 18:50:00 -0600

    cambios

--- src/Pages/Section4.jsx
+++ src/Pages/Section4.jsx
@@ -6,38 +6,41 @@ function Section4() {
   return (
     <div className='relative w-screen bg-copper-600' id='section-4'>
       < HeaderSection number={4} text="Outfit & Regalos" dark={true} />
-      <div className="flex flex-col items-center justify-center w-full h-screen mt-16 space-y-40">
-        <div className="flex items-center justify-center">
-          <div className="w-4/5 overflow-hidden rounded-lg bg-copper-100">
-            <div className="flex">
-              <div className="flex items-center justify-center w-3/5 p-4">
-                <h3 className='text-sm font-thin leading-loose text-left text-copper-600 md:text-5xl inspiration'> Opten por atuendos en los que se sientan elegantes y cómodos, reflejando su estilo único. Lo esencial es que irradien felicidad, al igual que nosotros. ¡Gracias por ser parte de nuestra historia y esta inolvidable celebración!</h3>
-
-              </div>
-              <div className="w-2/5 p-8 rounded-lg bg-copper-200">
+      <div className="w-full h-screen mt-16">
+        <div className='flex flex-col items-center justify-center md:space-y-36'>
+          <div className='flex-col items-center justify-center mx-auto md:flex'>
+            <div className="flex items-center justify-center md:w-2/3">
+              <div className="w-1/3">
                 <img
                   src="./img/vestido-de-novia.png"
                   alt="Imagen"
                   className="w-full h-full"
                 />
               </div>
+              <div className="flex items-center justify-center w-2/3 p-4">
+                <h3 className='text-sm font-thin leading-loose text-left text-white md:text-2xl '> Opten por atuendos en los que se sientan elegantes y cómodos, reflejando su estilo único. Lo esencial es que irradien felicidad, al igual que nosotros. ¡Gracias por ser parte de nuestra historia y esta inolvidable celebración!</h3>
 
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="w-4/5 overflow-hidden rounded-lg bg-copper-100">
-            <div className="flex">
-              <div className="flex items-center justify-center w-2/5 p-2 bg-copper-200">
+          <div className='flex-col items-center justify-center mx-auto md:flex'>
+            <div className="md:w-2/3 md:flex">
+
+              <div className="items-center justify-center p-4 md:flex md:w-2/3">
+                <h3 className='text-sm font-thin leading-loose text-left text-white md:text-2xl '>En este día de amor y unión, su presencia es el regalo más preciado. Si desean agregar un toque más, nuestra mesa de regalos está aquí para ustedes. ¡Gracias por compartir este día tan especial con nosotros!</h3>
+
+              </div>
+              <div className="flex items-center justify-center md:w-1/3">
                 <PiGiftThin className={"text-copper-100 text-9xl"} />
               </div>
-              <div className="w-3/5 p-4">
-                <h3 className='text-sm font-thin leading-loose text-left text-copper-600 md:text-5xl inspiration'>En este día de amor y unión, su presencia es el regalo más preciado. Si desean agregar un toque más, nuestra mesa de regalos está aquí para ustedes. ¡Gracias por compartir este día tan especial con nosotros!</h3>
-                <a href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51245484" target="_blank" rel="noopener noreferrer" className="w-full px-4 py-2 mt-6 text-xs text-center border rounded-full hover:text-white bg-copper-100 text-copper-600 border-copper-300 hover:bg-copper-700">mesa de regalos</a>
-              </div>
+            </div>
+            <div className='flex items-center justify-center '>
+
+              <a href="https://mesaderegalos.liverpool.com.mx/milistaderegalos/51245484" target="_blank" rel="noopener noreferrer" className="w-full px-4 py-2 mt-24 text-xs text-center border rounded-full hover:text-white bg-copper-100 text-copper-600 border-copper-300 hover:bg-copper-700 md:text-2xl">Ir a la mesa de regalos</a>
             </div>
           </div>
         </div>
+
       </div>
     </div>
   )
--- src/Pages/Section6.jsx
+++ src/Pages/Section6.jsx
@@ -7,8 +7,25 @@ export default function Section6() {
   return (
     <div className='relative w-screen bg-copper-600' id='section-6'>
       < HeaderSection number={6} text="Confirmar Asistencia" dark={true} />
-      <div className="flex items-center justify-center h-screen bg-white">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfiunqcAqiO88hbvT3OFz92-vdkfc0q5igcxnOJjIDdu5Y9wA/viewform?embedded=true" width="640" height="926" className='mt-12'>Cargando…</iframe>
+      <div className="flex items-center justify-center h-screen p-8">
+        <div className='w-2/3 text-sm text-white md:text-2xl lg:text-4xl'>
+          <h2>Su confirmación nos ayudará a garantizar que todo esté preparado para recibirlos de la mejor manera.
+
+          </h2>
+          <h2 className='mt-16'>
+
+            Si necesitan alguna información adicional de nuestra parte para la planificación o tienen alguna pregunta, no duden en hacérnoslo saber. Estamos entusiasmados por la oportunidad de celebrar junto a ustedes y crear recuerdos memorables en este día tan especial.
+
+
+          </h2>
+          <h2 className='mt-16'>
+
+            Por favor, envíennos su confirmación a la brevedad posible. ¡Esperamos con emoción el día y poder compartir con ustedes esta alegría!
+
+          </h2>
+        </div>
+        {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfiunqcAqiO88hbvT3OFz92-vdkfc0q5igcxnOJjIDdu5Y9wA/viewform?embedded=true" width="640" height="926" className='mt-12'>Cargando…</iframe> */}
+        
       </div>
       {/*  <div className="flex items-center justify-center mt-16 bg-gray-100 border h-[50vh]">
         {!show ? <button
